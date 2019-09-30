73402c964f8ac17565bfd0a32581f7b9e5efad88
Thomas <thomas.detemmerman@ugent.be>
2019-09-30 12:02:50 +0200

    chapter 1: outline of the issue

--- removeArtefacts.sh
+++
@@ -1,7 +0,0 @@
-
-declare -a artefacts=("aux" "log" "lol" "out" "lof" "lot" "toc")
-IFS='
-'
-for artefact in "${artefacts[@]}"; do
-    for x in `find . -name "*.${artefact}"`; do rm -r $x; done
-done;
