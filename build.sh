24036a717580d3e09623bbb8a208e3f508eadb63
Home <detemmerman.thomas@hotmail.com>
2019-10-17 16:08:26 +0200

    algoritm UL

--- build.sh
+++ build.sh
@@ -22,5 +22,8 @@ for artefact in "${artefacts[@]}"; do
     for x in `find . -name "*.${artefact}"`; do rm -r $x; done
 done;
 
-# Open pdf
+# Open pdf windows
+"/c/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/AcroRd32.exe" main.pdf
+
+# Open pdf mac
 open main.pdf
