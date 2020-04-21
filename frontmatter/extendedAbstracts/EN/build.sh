8305be381c25ce41c0719f568c4502d2edfee262
Thomas <thomas.detemmerman@ugent.be>
2020-04-21 18:57:38 +0200

    abstract ENG uitgebreid

--- frontmatter/extendedAbstracts/EN/build.sh
+++ frontmatter/extendedAbstracts/EN/build.sh
@@ -2,10 +2,17 @@
 # init build
 xelatex -shell-escape extendedabstract
 
+# build bibtex
+makeglossaries extendedabstract
+
+# build bibliography (requires a main.aux which is produced in stap 1)
+bibtex extendedabstract
 
 # build again
 xelatex -shell-escape extendedabstract
 
+# just to be sure
+xelatex -shell-escape extendedabstract
 
 # clean
 declare -a artefacts=("aux" "log" "lol" "out" "lof" "lot" "toc" "acn" "acr" "alg" "glg" "glo" "gls" "glsdefs" "ist" "bbl" "bcf" "blg" "run.xml")
